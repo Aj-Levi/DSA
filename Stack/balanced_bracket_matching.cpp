cdca9404f6e9ff6fb2a48be8c9e7978cf487aa0d
ABHIJEET JAIN <abhijeetjain1413@gmail.com>
2025-03-13 15:42:21 +0530

    Added Valid Paranthesis and Next Greater Element

---
+++ Stack/balanced_bracket_matching.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string is_balanced(string str){
+    unordered_map<char,int>m;
+    m.insert(make_pair('[',-1));
+    m.insert(make_pair('{',-2));
+    m.insert(make_pair('(',-3));
+    m.insert(make_pair(']',1));
+    m.insert(make_pair('}',2));
+    m.insert(make_pair(')',3));
+    stack<char>st;
+    for(auto &value : str){
+        if(m[value]<0){
+            st.push(value);
+        }else{
+            if(st.empty()){
+                return "NO";
+            }else{
+                if((m[value]+m[st.top()])==0){
+                    st.pop();
+                }else{
+                    return "NO";
+                }
+            }
+        }
+    }
+    if(st.empty()){
+        return "YES";
+    }else{
+        return "NO";
+    }
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string str;
+        cin >> str;
+        cout << is_balanced(str) << endl;
+    }
+
+}
