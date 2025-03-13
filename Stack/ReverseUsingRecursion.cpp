9bfae73be41346967df9dc0cc50be34ec9cd6fdd
ABHIJEET JAIN <abhijeetjain1413@gmail.com>
2025-03-13 16:14:26 +0530

    Refactor ReverseUsingRecursion.cpp by removing unused code and adding time complexity comments

--- Stack/ReverseUsingRecursion.cpp
+++ Stack/ReverseUsingRecursion.cpp
@@ -13,60 +13,11 @@
 #include <unordered_map>
 using namespace std;
 #define ll long long
-#define mp make_pair
-#define pb push_back
-#define vll vector<ll>
-#define all(v) v.begin(),v.end()
 #define fastio() \
 ios_base::sync_with_stdio(false); \
 cin.tie(nullptr)
-const int M = 1e9 + 7;
 
-inline void py() {
-    cout << "YES" << endl;
-}
-inline void pn() {
-    cout << "NO" << endl;
-}
-
-ll binmul(ll a, ll b) {
-    ll ans = 0;
-    while (b) {
-        if (b & 1) ans = (ans + a) % M;
-        a = (a + a) % M;
-        b >>= 1;
-    }
-    return ans;
-}
-
-ll bin_exp(ll a, ll b) {
-    ll ans = 1;
-    a %= M;
-    while (b) {
-        if (b & 1) ans = binmul(ans, a);
-        a = binmul(a, a);
-        b >>= 1;
-    }
-    return ans;
-}
-
-void pv(vll &v) {
-    for (auto &val : v) {
-        cout << val << ' ';
-    }
-    cout << endl;
-}
-
-vll vin(ll n) {
-    vll temp;
-    for (int i = 0; i < n; i++) {
-        ll x;
-        cin >> x;
-        temp.pb(x);
-    }
-    return temp;
-}
-
+// this function has O(n) TC and O(n) SC
 void insert_At_Bottom(stack<int> &stk,int val){
     if(stk.empty()){
         stk.push(val);
@@ -81,6 +32,7 @@ void insert_At_Bottom(stack<int> &stk,int val){
     stk.push(num);
 }
 
+// this function has O(n^2) TC and O(n) SC
 void rev(stack<int> &stk){
     if(stk.empty()) return;
 
